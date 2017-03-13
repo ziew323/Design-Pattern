1de5c0ed27996551c9dcb3cf9f4fbd918fdc6082
JIAZIEW\DARE2DREAM <jiaziew@gmail.com>
2017-03-13 23:19:42 +0800

    MyList

--- Test/src/base/collection/MyLinkedList.java
+++ Test/src/base/collection/MyLinkedList.java
@@ -3,7 +3,11 @@ package base.collection;
 public class MyLinkedList implements MyList {
 
     public static void main(String[] args) {
-
+        MyList list = new MyLinkedList();
+        list.add(1);
+        list.add("2");
+        System.out.println(list.toString());
+        System.out.println(list.contains(2));
     }
 
     private int size;
@@ -13,11 +17,17 @@ public class MyLinkedList implements MyList {
     private Node last;
 
     private class Node {
+        public Node(Node prev, Object element, Node next) {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+
         Object element;
 
-        Node nextNode;
+        Node next;
 
-        Node prevNode;
+        Node prev;
     }
 
     @Override
@@ -32,17 +42,47 @@ public class MyLinkedList implements MyList {
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        if (indexOf(o) < 0) {
+            return false;
+        }
+        return true;
     }
 
     private int indexOf(Object o) {
-        return 0;
+        Node node = first;
+        if (node == null) {
+            return -1;
+        }
+        if (o == null) {
+            for (int i = 0; node.next != null; node = node.next) {
+                if (node.element == null) {
+                    return i;
+                }
+                i++;
+            }
+        } else {
+            for (int i = 0; node.next != null; node = node.next) {
+                if (o.equals(node.element)) {
+                    return i;
+                }
+                i++;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean add(Object o) {
-        // TODO Auto-generated method stub
-        return false;
+        Node l = last;
+        Node newNode = new Node(l, o, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        return true;
     }
 
     @Override
@@ -73,6 +113,20 @@ public class MyLinkedList implements MyList {
     public Object remove(int index) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("MyLinkedList {size=").append(size)
+            .append(", elementData=[");
+        for (Node node = first; node != null; node = node.next) {
+            stringBuilder.append(node.element);
+            if (node.next != null) {
+                stringBuilder.append(",");
+            }
+        }
+        stringBuilder.append("]}");
+        return stringBuilder.toString();
     }
 
 }
